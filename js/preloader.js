4100700ca33ce55f4a0ac815502ab24f35a28940
imambungo <imambungo@gmail.com>
2018-06-07 14:20:01 +0700

    fixed overflow hidden

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,7 @@
 //<![CDATA[
 $(window).on('load', function () { // makes sure the whole site is loaded 
 	$('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-	$('body').delay(250).css({
-		'overflow': 'hidden'
-	});
+	// $('body').delay(250).css({
+	// });
 })
 //]]>
