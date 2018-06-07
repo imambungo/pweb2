2d80b0bcdadd2fb08666a2c1d2fc829c99a8f100
imambungo <imambungo@gmail.com>
2018-06-07 10:48:56 +0700

    biar keliatan di hp

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,7 @@
 //<![CDATA[
 $(window).on('load', function () { // makes sure the whole site is loaded 
-	$('#preloader').delay(50).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-	$('body').delay(50).css({
+	$('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	$('body').delay(250).css({
 		'overflow': 'hidden'
 	});
 })
